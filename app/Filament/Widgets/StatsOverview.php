4af77610c705f9c145c6bc7d2b215473ad4afba4
osama-zayed <o.a.s775561590@gmail.com>
2024-07-25 02:12:45 +0300

    first commit

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -2,9 +2,11 @@
 
 namespace App\Filament\Widgets;
 
-use App\Models\Driver;
-use App\Models\Factory;
-use App\Models\Family;
+use App\Models\Activity;
+use App\Models\Chain;
+use App\Models\Domain;
+use App\Models\Procedure;
+use App\Models\Project;
 use App\Models\User;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
@@ -13,37 +15,38 @@ class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
-        // $driverCount = Driver::count();
-        // $factoryCount = Factory::count();
-        // $familyCount = Family::count();
-        // $associationCount = User::where("user_type", 'association')->count();
-        // $representativeCount = User::where("user_type", 'representative')->count();
-        // $collectorCount = User::where("user_type", 'collector')->count();
+        $UserCount = User::where('user_type','user')->count();
+        $DomainCount = Domain::count();
+        $ProjectCount = Project::count();
+        $ChainCount = Chain::count();
+        $ActivityCount = Activity::count();
+        $ProcedureCount = Procedure::count();
+       
         return [
-            // Stat::make('عدد الجمعيات', $associationCount)
-            //     ->descriptionIcon('heroicon-m-arrow-trending-up')
-            //     ->chart([15, 2, 5, 3, 15, 4, 5])
-            //     ->color('info'),
-            // Stat::make('عدد المناديب', $representativeCount)
-            //     ->descriptionIcon('heroicon-m-arrow-trending-up')
-            //     ->chart([15, 5, 10, 3, 5, 5, 15])
-            //     ->color('info'),
-            // Stat::make('عدد المجمعين', $collectorCount)
-            //     ->descriptionIcon('heroicon-m-arrow-trending-up')
-            //     ->chart([7, 2, 10, 3, 15, 4, 17])
-            //     ->color('info'),
-            // Stat::make('عدد المصانع', $factoryCount)
-            //     ->descriptionIcon('heroicon-m-arrow-trending-up')
-            //     ->chart([170, 160, 140, 100, 140, 130, 120])
-            //     ->color('info'),
-            // Stat::make('عدد السائقين', $driverCount)
-            //     ->descriptionIcon('heroicon-m-arrow-trending-up')
-            //     ->chart([1, 5, 10, 3, 15, 4, 17])
-            //     ->color('info'),
-            // Stat::make('عدد الاسر المنتجه', $familyCount)
-            //     ->descriptionIcon('heroicon-m-arrow-trending-up')
-            //     ->chart([2, 8, 15, 3, 15, 0, 7])
-            //     ->color('info'),
+            Stat::make('عدد المستخدمين', $UserCount)
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->chart([15, 2, 5, 3, 15, 4, 5])
+                ->color('info'),
+            Stat::make('عدد المجالات', $DomainCount)
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->chart([15, 5, 10, 3, 5, 5, 15])
+                ->color('info'),
+            Stat::make('عدد السلاسل', $ChainCount)
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->chart([15, 5, 10, 3, 5, 5, 15])
+                ->color('info'),
+            Stat::make('عدد المشاريع', $ProjectCount)
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->chart([7, 2, 10, 3, 15, 4, 17])
+                ->color('info'),
+            Stat::make('عدد الانشطة', $ActivityCount)
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                ->chart([170, 160, 140, 100, 140, 130, 120])
+                ->color('info'),
+            Stat::make('عدد الاجراءات', $ProcedureCount)
+                ->descriptionIcon('heroicon-m-arrow-trending-up')
+                    ->chart([1, 5, 10, 3, 15, 4, 17])
+                ->color('info'),
         ];
     }
 }

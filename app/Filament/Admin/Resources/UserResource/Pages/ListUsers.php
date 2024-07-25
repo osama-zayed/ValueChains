4e8e96710e61ef8914575a4ca6f181e6f55e7665
osama-zayed <o.a.s775561590@gmail.com>
2024-07-25 21:12:40 +0300

    first commit

--- app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
+++ app/Filament/Admin/Resources/UserResource/Pages/ListUsers.php
@@ -6,6 +6,7 @@ use App\Filament\Admin\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
+
 class ListUsers extends ListRecords
 {
     protected static string $resource = UserResource::class;
@@ -19,15 +20,10 @@ class ListUsers extends ListRecords
     protected function getTableQuery(): Builder
     {
         $query = parent::getTableQuery();
-    
-        // Exclude the currently logged-in user from the table
         if ($user = auth()->user()) {
             $query->where('id', '!=', $user->id);
         }
-    
-        // Filter the table to only show users with the 'collector' role
-        $query->where('user_type','user');
-    
+        $query->where('user_type', 'user');
         return $query;
     }
 }
